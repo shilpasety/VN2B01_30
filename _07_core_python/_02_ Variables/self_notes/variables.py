dc4c4f92d205e499c7f9c9252a82987ee294230d
shilpa sety <shilpasetyroyal@gmail.com>
2021-12-22 17:24:25 +0530

    22 Dec 21

---
+++ _07_core_python/_02_ Variables/self_notes/variables.py
@@ -0,0 +1,27 @@
+'''
+Variable names rules
+'''
+
+_123 = 10
+print(_123)
+
+xyz = 'Shilpa'
+print(xyz)
+
+_a12 = 'c'
+print(_a12)
+
+x=True
+print(id(x))
+print(type(x))
+
+f = 20
+print(id(f))
+g=f
+print(id(g))
+print(id(20))
+
+f=80
+print(id(f))
+print(id(g))
+print(id(80))
