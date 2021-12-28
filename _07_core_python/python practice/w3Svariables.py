361d38d652e09aad224e600394a5e5177944b830
shilpa sety <shilpasetyroyal@gmail.com>
2021-12-28 17:26:45 +0530

    28-12-21

---
+++ _07_core_python/python practice/w3Svariables.py
@@ -0,0 +1,25 @@
+x,y,z=10,20,30  # We can assign multiple values to variables in single line.
+print (x)
+print(y)
+print(z)
+print('---------------')
+
+print('Assigning the same value to differenct variables:')
+e=r=t=5
+print(e,r,t)
+
+print('-----------------')
+print('Unpacking a collection:\n')
+a,b,c= ['Shilpa','Advaitha','Pranaya'] # Unpacking a collection
+print(a)
+print(b)
+print(c)
+
+print('---------------------')
+print('Adding two variables using + symbol')
+a='Shilpa '
+b='Advaitha'
+print(a+b)
+
+print('--------------------')
+
---
+++ _07_core_python/python practice/while loop.py
@@ -0,0 +1,43 @@
+'''
+print the pattern
+Pranaya Advaitha Advaitha Advaitha Advaitha
+Pranaya Advaitha Advaitha Advaitha Advaitha
+Pranaya Advaitha Advaitha Advaitha Advaitha
+Pranaya Advaitha Advaitha Advaitha Advaitha
+'''
+
+'''p = 1
+a = 1
+while(p<=4):
+     print("Pranaya ",end="")
+     a=1
+     while(a<=4):
+          print("Advaitha ",end="")
+          a+=1
+     p+=1     # p+=1 == p=p+1
+     print()  '''
+
+'''
+Print the pattern
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+# # # # #
+'''
+
+'''k = 1
+while(k<=5):
+     print("# ",end="")
+     n=1
+     while(n<=4):
+          print("# ",end="")
+          n+=1
+     k+=1
+     print()'''
+
+k=1
+while(k<=100):
+    if(k%3!=0 and k%5!=0):   # skip the numbers divisible by 3 and 5
+        print(k)
+    k+=1
